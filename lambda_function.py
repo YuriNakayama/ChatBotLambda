bd1ebb2c4f007aa15e71ecf30fcbd1eee298a953
YuriNakayama <yuri620620@gmail.com>
2023-03-28 15:15:23 +0000

    change return

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({'message': 'Hello, World!'})
+    return make_response(jsonify({'message': 'Hello, World!'}))
 
 def lambda_handler(event, context):
     return app
