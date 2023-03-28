e1b1b35a816aa656fa80dc7ccfda1737e913a994
YuriNakayama <yuri620620@gmail.com>
2023-03-28 14:19:26 +0000

    add requirements.txt, change script to Flask application

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,7 @@
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello, World!'
-    }
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
