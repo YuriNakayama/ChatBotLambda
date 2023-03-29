22ca939ab8568cc89b654be8802497ba78e31982
YuriNakayama <yuri620620@gmail.com>
2023-03-29 04:24:35 +0000

    delete .env

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,12 @@
+from flask import Flask, jsonify, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    response = make_response(jsonify({'message': 'Hello, World!'}))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello, World!'
-    }
+    return app
