f8c11906bc29cad33bbd48b5b552667006801b9c
YuriNakayama <yuri620620@gmail.com>
2023-03-29 05:28:03 +0000

    change requirements.txt

--- Test/test_generate_reply.py
+++ Test/test_generate_reply.py
@@ -1,8 +1,14 @@
 import sys
+import pytest
 from os.path import abspath, dirname
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
 from lambda_function import generate_reply
+
+@pytest.fixture
+def load_env():
+    from dotenv import load_dotenv
+    load_dotenv()
 
 def test_generate_reply():
     user_message = "Hello, world!"
