d76e76ac9f5d52d9e19358fbba5c27a7b108683a
YuriNakayama <yuri620620@gmail.com>
2023-03-29 05:25:36 +0000

    change requirements.txt

--- Test/test_generate_reply.py
+++ Test/test_generate_reply.py
@@ -1,3 +1,9 @@
+import sys
+from os.path import abspath, dirname
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from lambda_function import generate_reply
+
 def test_generate_reply():
     user_message = "Hello, world!"
     reply = generate_reply(user_message)
